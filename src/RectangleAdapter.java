a9edec94bb2448e545141365360e5193d2436496
sjacobsTU <sjacobs21@touro.edu>
2025-04-27 11:26:59 -0400

    Restored RectangleAdapter.java

--- src/RectangleAdapter.java
+++ src/RectangleAdapter.java
@@ -8,7 +8,11 @@
 
         // Convert (x1, y1), (x2, y2) format to (x, y, width, height)
         public void draw(int x1, int y1, int x2, int y2) {
-
+            int x = Math.min(x1, x2);
+            int y = Math.min(y1, y2);
+            int width = Math.abs(x2 - x1);
+            int height = Math.abs(y2 - y1);
+            legacyRectangle.draw(x, y, width, height);
         }
     }
 
